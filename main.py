d8f702805033c6ec1e10d510b881f73f41cc1c45
ender-orwell <enderOrwell@outlook.com>
2025-03-01 18:28:53 +0100

    quiz completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 def main():
     question_bank = []
@@ -10,8 +11,14 @@ def main():
         new_question = Question(question_text, question_answer)
         question_bank.append(new_question)
 
-    print(question_bank)
+    quiz = QuizBrain(question_bank)
+    quiz.next_question()
 
+    while quiz.still_has_questions():
+        quiz.next_question()
+
+    print("You have comleted the quiz!!")
+    print(f"Your final score was: {quiz.score}/{quiz.question_number}")
 
 if __name__ == '__main__':
     main()
